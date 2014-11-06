a595e3addd2cd054079c904ad531bf64246dd7f6
Ze'ev <lailari.zeev@gmail.com>
2014-11-05 23:35:08 -0600

    added some timer stuff to get speed

--- basicCounter.c
+++ basicCounter.c
@@ -1,14 +1,20 @@
+
+#include <Time.h>  
+
 // These constants won't change.  They're used to give names
 // to the pins used:
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
 
 int sensorValue = 0;        // value read from the pot
 int count = 0;
+int timeStart = 0;
+int timeEnd = 0;
 boolean up = false;
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600); 
+  setTime(0);
 }
 
 void loop() {
@@ -17,12 +23,23 @@ void loop() {
 
   // print the results to the serial monitor:
   Serial.print("sensor = " );                       
-  Serial.println(sensorValue);
+  Serial.print(sensorValue);
+  Serial.print(", ");
   
-  if(sensorValue>41 && up==false) {
+  if(sensorValue>41) {
+    Serial.println("");
+    if(timeStart > 0) {
+      timeEnd = now(); 
+      int diff = timeStart - timeEnd;
+      Serial.println("Diff is "+diff);
+      timeStart = 0;
+      timeEnd = 0; 
+    }
+    else {
+      timeStart = now();
+    }
     up = true;
     count = count + 1;
-     
   }
   if(sensorValue<=38 && up==true) {
     up = false;  
